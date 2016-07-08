e9c9075150521f27f108a2c761e680141338f1e5
Cédric Krommenhoek <ckrommenhoek@osivia.com>
2016-07-08 13:16:11 +0000

    Procedure services 4.4.0 migration.

--- src/main/java/org/osivia/services/procedure/plugin/ProcedurePlugin.java
+++ src/main/java/org/osivia/services/procedure/plugin/ProcedurePlugin.java
@@ -14,34 +14,17 @@
 package org.osivia.services.procedure.plugin;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.portlet.GenericPortlet;
-import javax.portlet.PortletException;
-
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.osivia.portal.api.PortalException;
 import org.osivia.portal.api.cms.DocumentType;
-import org.osivia.portal.api.context.PortalControllerContext;
 import org.osivia.portal.api.customization.CustomizationContext;
-import org.osivia.portal.api.customization.ICustomizationModule;
-import org.osivia.portal.api.internationalization.IBundleFactory;
-import org.osivia.portal.api.internationalization.IInternationalizationService;
-import org.osivia.portal.api.locator.Locator;
-import org.osivia.portal.api.menubar.MenubarGroup;
-import org.osivia.portal.api.menubar.MenubarItem;
+import org.osivia.portal.api.menubar.MenubarModule;
 import org.osivia.portal.api.player.IPlayerModule;
-import org.osivia.portal.core.cms.CMSException;
-import org.osivia.portal.core.cms.CMSExtendedDocumentInfos;
-import org.osivia.portal.core.cms.CMSPublicationInfos;
-import org.osivia.portal.core.cms.CMSServiceCtx;
 
 import fr.toutatice.portail.cms.nuxeo.api.domain.AbstractPluginPortlet;
-import fr.toutatice.portail.cms.nuxeo.api.domain.IMenubarModule;
 import fr.toutatice.portail.cms.nuxeo.api.domain.ListTemplate;
 
 
@@ -49,10 +32,9 @@ import fr.toutatice.portail.cms.nuxeo.api.domain.ListTemplate;
  * Technical portlet for attributes bundles customization.
  *
  * @author Jean-Sébastien steux
- * @see GenericPortlet
- * @see ICustomizationModule
+ * @see AbstractPluginPortlet
  */
-public class ProcedurePlugin extends AbstractPluginPortlet implements ICustomizationModule, IMenubarModule{
+public class ProcedurePlugin extends AbstractPluginPortlet {
 
     /** Customizer name. */
     private static final String CUSTOMIZER_NAME = "procedure.plugin";
@@ -67,28 +49,14 @@ public class ProcedurePlugin extends AbstractPluginPortlet implements ICustomiza
     /** Logger. */
     private static final Log LOGGER = LogFactory.getLog(ProcedurePlugin.class);
 
-    /** Bundle factory. */
-    protected IBundleFactory bundleFactory;
-
 
     /**
      * Constructor.
      */
     public ProcedurePlugin() {
         super();
-
     }
 
-
-    @Override
-    public void init() throws PortletException {
-        super.init();
-
-        // Bundle factory
-        IInternationalizationService internationalizationService = Locator.findMBean(IInternationalizationService.class,
-                IInternationalizationService.MBEAN_NAME);
-        bundleFactory = internationalizationService.getBundleFactory(this.getClass().getClassLoader());
-    }
 
     /**
      * {@inheritDoc}
@@ -113,34 +81,12 @@ public class ProcedurePlugin extends AbstractPluginPortlet implements ICustomiza
         // ! insertion au début
         modules.add(0, new ProcedurePlayer(getPortletContext()));
 
-        List<IMenubarModule> menubars = getMenubars(context);
-        menubars.add(this);
-    }
 
+        // Menubar modules
+        List<MenubarModule> menubarModules = this.getMenubarModules(context);
 
-    @Override
-    public void adaptContentMenuBar(CMSServiceCtx C, List<MenubarItem> menuBar, CMSPublicationInfos publicationInfos,
-            CMSExtendedDocumentInfos extendedDocumentInfos) throws CMSException {
-
-        String style = (String) C.getRequest().getAttribute("style");
-        if (StringUtils.equals(style, "adminproc")) {
-            PortalControllerContext pcc = new PortalControllerContext(getPortletContext(), C.getRequest(), C.getResponse());
-
-            Map<String, String> windowProperties = new HashMap<String, String>();
-            windowProperties.put("osivia.title", getMessage(pcc, "CREATE_PROCEDURE_ACTION"));
-            windowProperties.put("osivia.procedure.admin", style);
-            try {
-                String urlCreate = getPortalUrlFactory().getStartPortletUrl(pcc, "osivia-services-procedure-portletInstance", windowProperties, false);
-                MenubarItem manageMembersItem = new MenubarItem("CREATE_PROCEDURE", getMessage(pcc, "CREATE_PROCEDURE_ACTION"),
-                        "glyphicons glyphicons-circle-plus", MenubarGroup.SPECIFIC, 22, urlCreate, null, null, null);
-
-                menuBar.add(manageMembersItem);
-
-            } catch (PortalException e) {
-                LOGGER.warn(e.getMessage());
-            }
-        }
-
+        MenubarModule menubarModule = new ProcedureMenubarModule();
+        menubarModules.add(menubarModule);
     }
 
 
