b7db379cf778f455ecafc505394a3c3e03e2abfb
Loïc Billon <lbillon@osivia.com>
2016-09-22 16:20:41 +0000

    backport phase 3

--- src/main/java/org/osivia/services/procedure/plugin/ProcedurePlugin.java
+++ src/main/java/org/osivia/services/procedure/plugin/ProcedurePlugin.java
@@ -15,16 +15,11 @@ package org.osivia.services.procedure.plugin;
 
 import java.util.Map;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-//import org.osivia.portal.api.cms.DocumentType;
 import org.osivia.portal.api.customization.CustomizationContext;
 import org.osivia.services.procedure.formFilters.DefineVariableFilter;
 import org.osivia.services.procedure.formFilters.IfFilter;
-//import org.osivia.portal.api.menubar.MenubarModule;
-//import org.osivia.portal.api.player.IPlayerModule;
 import org.osivia.services.procedure.formFilters.SendMailFilter;
-import org.osivia.services.procedure.formFilters.SetVariableAsActorFilter;
+import org.osivia.services.procedure.formFilters.SetActorFormFilter;
 import org.osivia.services.procedure.formFilters.ThrowExceptionFilter;
 
 import fr.toutatice.portail.cms.nuxeo.api.domain.AbstractPluginPortlet;
@@ -49,72 +44,53 @@ public class ProcedurePlugin extends AbstractPluginPortlet {
     /** SCHEMAS_ADMIN */
     public static final String SCHEMAS_ADMIN = "dublincore, common, toutatice";
 
-    /** Logger. */
-    private static final Log LOGGER = LogFactory.getLog(ProcedurePlugin.class);
-
-
-    private final FormFilter sendMailFilter;
-
-    private final FormFilter ifFilter;
-
-    private final FormFilter setVariableAsActorFilter;
-
-    private final FormFilter defineVariableFilter;
-
-    private final FormFilter throwException;
-
 
     /**
      * Constructor.
      */
     public ProcedurePlugin() {
         super();
-        sendMailFilter = new SendMailFilter();
-        ifFilter = new IfFilter();
-        setVariableAsActorFilter = new SetVariableAsActorFilter();
-        defineVariableFilter = new DefineVariableFilter();
-        throwException = new ThrowExceptionFilter();
     }
 
 
     /**
      * {@inheritDoc}
      */
+    @SuppressWarnings("rawtypes")
     @Override
     protected void customizeCMSProperties(String customizationID, CustomizationContext context) {
 
-        Map<String, FormFilter> filters = getFormFilters(context);
+//        Map<String, DocumentType> docTypes = getDocTypes(context);
+//
+//        ArrayList<String> portalFormSubTypes = new ArrayList<String>(0);
+//        docTypes.put("ProcedureModel", new DocumentType("ProcedureModel", false, false, false, false, false, false, portalFormSubTypes, null,
+//                "glyphicons glyphicons-flowchart"));
+//
+//
+//        Map<String, ListTemplate> templates = getListTemplates(context);
+//
+//        ListTemplate picturebookTemplate = new ListTemplate(STYLE_ADMIN, "admin téléprocédure", SCHEMAS_ADMIN);
+//        templates.put(STYLE_ADMIN, picturebookTemplate);
+//
+//
+//        List<IPlayerModule> modules = getPlayers(context);
+//        // ! insertion au début
+//        modules.add(0, new ProcedurePlayer(getPortletContext()));
+//
+//
+//        // Menubar modules
+//        List<MenubarModule> menubarModules = this.getMenubarModules(context);
+//
+//        MenubarModule menubarModule = new ProcedureMenubarModule();
+//        menubarModules.add(menubarModule);
 
-        filters.put(SendMailFilter.ID, sendMailFilter);
-        filters.put(IfFilter.ID, ifFilter);
-        filters.put(SetVariableAsActorFilter.ID, setVariableAsActorFilter);
-        filters.put(DefineVariableFilter.ID, defineVariableFilter);
-        filters.put(ThrowExceptionFilter.ID, throwException);
 
-        //        Map<String, DocumentType> docTypes = getDocTypes(context);
-        //
-        //        ArrayList<String> portalFormSubTypes = new ArrayList<String>(0);
-        //        docTypes.put("ProcedureModel", new DocumentType("ProcedureModel", false, false, false, false, false, false, portalFormSubTypes, null,
-        //                "glyphicons glyphicons-flowchart"));
-        //
-        //
-        //        Map<String, ListTemplate> templates = getListTemplates(context);
-        //
-        //        ListTemplate picturebookTemplate = new ListTemplate(STYLE_ADMIN, "admin téléprocédure", SCHEMAS_ADMIN);
-        //        templates.put(STYLE_ADMIN, picturebookTemplate);
-        //
-        //
-        //        List<IPlayerModule> modules = getPlayers(context);
-        //        // ! insertion au début
-        //        modules.add(0, new ProcedurePlayer(getPortletContext()));
-        //
-        //
-        //        // Menubar modules
-        //        List<MenubarModule> menubarModules = this.getMenubarModules(context);
-        //
-        //        MenubarModule menubarModule = new ProcedureMenubarModule();
-        //        menubarModules.add(menubarModule);
-
+        Map<String, FormFilter> formFilters = getFormFilters(context);
+        formFilters.put(IfFilter.ID, new IfFilter());
+        formFilters.put(DefineVariableFilter.ID, new DefineVariableFilter());
+        formFilters.put(SendMailFilter.ID, new SendMailFilter());
+        formFilters.put(SetActorFormFilter.ID, new SetActorFormFilter());
+        formFilters.put(ThrowExceptionFilter.ID, new ThrowExceptionFilter());
     }
 
 
